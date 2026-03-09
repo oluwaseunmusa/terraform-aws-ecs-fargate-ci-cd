952d165254ce274a8820650c1ce7b6f2b0c68a7f
Seun Musa <seunmusa@Seuns-MacBook-Pro.local>
2026-03-08 20:35:47 -0500

    Fixed pipeline connection to AWS environment

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket  = "my-terraform-state-bucket"
+    bucket  = "terraform-state-aws-ecs-fargate"
     key     = "github-actions/terraform.tfstate"
     region  = "us-east-1"
     encrypt = true
